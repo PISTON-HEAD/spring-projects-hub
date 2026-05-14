49bc869ec814d3cfbb065a35092d1f8679b03f70
PISTON-HEAD <gokulkrishna1282@gmail.com>
2026-05-15 00:40:14 +0530

    first checkIn for the new Microservice application (Patient Service)

---
+++ healthcare-hospital-system/patient-service/src/main/java/com/healthcare/hospital_booking_system/HospitalBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.healthcare.hospital_booking_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalBookingSystemApplication.class, args);
+	}
+
+}
---
+++ healthcare-hospital-system/patient-service/src/test/java/com/healthcare/hospital_booking_system/HospitalBookingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare.hospital_booking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalBookingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
