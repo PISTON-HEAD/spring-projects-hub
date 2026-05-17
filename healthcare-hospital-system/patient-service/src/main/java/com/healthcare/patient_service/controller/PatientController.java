05d7a8aaeb6f04f06651a49e988057e0d8ca6d1e
PISTON-HEAD <gokulkrishna1282@gmail.com>
2026-05-17 22:40:55 +0530

    creating patient controller.java

---
+++ healthcare-hospital-system/patient-service/src/main/java/com/healthcare/patient_service/controller/PatientController.java
@@ -0,0 +1,13 @@
+package com.healthcare.patient_service.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/patients")
+@RequiredArgsConstructor
+public class PatientController {
+
+}
