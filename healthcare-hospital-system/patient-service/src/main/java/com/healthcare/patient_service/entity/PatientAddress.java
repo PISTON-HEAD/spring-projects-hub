71addc84b519a392f728fd7b8219216726b4c79a
PISTON-HEAD <gokulkrishna1282@gmail.com>
2026-05-18 02:07:41 +0530

    made some chnages to the yaml file

--- healthcare-hospital-system/patient-service/src/main/java/com/healthcare/patient_service/entity/PatientAddress.java
+++ healthcare-hospital-system/patient-service/src/main/java/com/healthcare/patient_service/entity/PatientAddress.java
@@ -2,7 +2,6 @@ package com.healthcare.patient_service.entity;
 
 import java.util.UUID;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
