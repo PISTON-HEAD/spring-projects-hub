2db9a6eb4c9295ba9288ec2227e9683231a96e5f
PISTON-HEAD <gokulkrishna1282@gmail.com>
2026-05-20 00:06:14 +0530

    Added the necessary feilds for the doctor entity and also the created the ENUM for the doctor availability slot

---
+++ healthcare-hospital-system/doctor-service/src/main/java/com/healthcare/doctor_service/entity/Doctor.java
@@ -0,0 +1,56 @@
+package com.healthcare.doctor_service.entity;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import com.google.auto.value.AutoValue.Builder;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Doctor {
+ @Id
+ @GeneratedValue(strategy = GenerationType.UUID)
+ private UUID id;
+
+ @Column(nullable = false)
+ private String firstName;
+
+ private String lastName;
+
+ @Column(nullable = false)
+ private String specialization;
+
+ @Column(nullable = false, unique = true)
+ private String email;
+
+ private String phoneNumber;
+
+ @Column(nullable = false)
+ private Boolean active;
+
+ @Column(nullable = false)
+ private LocalDateTime createdAt;
+
+ @PrePersist
+ public void setCreatedAt() {
+  this.createdAt = LocalDateTime.now();
+  if (this.active == null) {
+   this.active = true;
+  }
+ }
+}
