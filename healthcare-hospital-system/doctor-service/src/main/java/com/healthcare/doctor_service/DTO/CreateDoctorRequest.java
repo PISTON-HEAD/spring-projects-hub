0adde33f6f33979dd2c2d1a35bdef69f309dea21
PISTON-HEAD <gokulkrishna1282@gmail.com>
2026-05-21 22:59:49 +0530

    Doctor DTO creations

--- healthcare-hospital-system/doctor-service/src/main/java/com/healthcare/doctor_service/DTO/CreateDoctorRequest.java
+++ healthcare-hospital-system/doctor-service/src/main/java/com/healthcare/doctor_service/DTO/CreateDoctorRequest.java
@@ -1,4 +1,4 @@
-package com.healthcare.doctor_service.DTO;
+package com.healthcare.doctor_service.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
