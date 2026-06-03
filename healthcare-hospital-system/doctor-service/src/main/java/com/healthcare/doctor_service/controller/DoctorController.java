276aac8ac5884912e7303dc3910c2e9f29b60c92
PISTON-HEAD <gokulkrishna1282@gmail.com>
2026-06-04 01:09:08 +0530

    some minor changes on doctor service for running the docker compose properly wthout errors

--- healthcare-hospital-system/doctor-service/src/main/java/com/healthcare/doctor_service/controller/DoctorController.java
+++ healthcare-hospital-system/doctor-service/src/main/java/com/healthcare/doctor_service/controller/DoctorController.java
@@ -9,7 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.healthcare.doctor_service.dto.CreateDoctorRequest;
 import com.healthcare.doctor_service.service.DoctorService;
