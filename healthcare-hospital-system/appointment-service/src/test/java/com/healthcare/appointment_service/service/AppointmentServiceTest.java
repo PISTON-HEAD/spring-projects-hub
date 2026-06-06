82fa06117a31bf0985997fe464d4f3dca72caf8b
PISTON-HEAD <gokulkrishna1282@gmail.com>
2026-06-06 12:51:00 +0530

    added testcase for private method

--- healthcare-hospital-system/appointment-service/src/test/java/com/healthcare/appointment_service/service/AppointmentServiceTest.java
+++ healthcare-hospital-system/appointment-service/src/test/java/com/healthcare/appointment_service/service/AppointmentServiceTest.java
@@ -4,6 +4,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
@@ -33,85 +35,95 @@ import com.healthcare.patient_service.grpc.PatientResponse;
 @ExtendWith(MockitoExtension.class)
 public class AppointmentServiceTest {
 
- @Mock
- private AppointmentRepository repository;
+  @Mock
+  private AppointmentRepository repository;
 
- @Mock
- private PatientGrpcServiceGrpc.PatientGrpcServiceBlockingStub patientStub;
+  @Mock
+  private PatientGrpcServiceGrpc.PatientGrpcServiceBlockingStub patientStub;
 
- @Mock
- private DoctorGrpcServiceGrpc.DoctorGrpcServiceBlockingStub doctorStub;
+  @Mock
+  private DoctorGrpcServiceGrpc.DoctorGrpcServiceBlockingStub doctorStub;
 
- @InjectMocks
- private AppointmentService service;
+  @InjectMocks
+  private AppointmentService service;
 
- private Appointment appointment;
+  private Appointment appointment;
 
- private CreateAppointmentRequest request;
+  private CreateAppointmentRequest request;
 
- @BeforeEach
- public void beforeEach() {
+  @BeforeEach
+  public void beforeEach() {
 
-  ReflectionTestUtils.setField(service, "patientStub", patientStub);
-  ReflectionTestUtils.setField(service, "doctorStub", doctorStub);
+    ReflectionTestUtils.setField(service, "patientStub", patientStub);
+    ReflectionTestUtils.setField(service, "doctorStub", doctorStub);
 
-  UUID patientId = UUID.randomUUID();
-  UUID doctorId = UUID.randomUUID();
-  UUID slotId = UUID.randomUUID();
+    UUID patientId = UUID.randomUUID();
+    UUID doctorId = UUID.randomUUID();
+    UUID slotId = UUID.randomUUID();
 
-  LocalDateTime now = LocalDateTime.of(2002, 8, 12, 8, 40, 00);
+    LocalDateTime now = LocalDateTime.of(2002, 8, 12, 8, 40, 00);
 
-  appointment = Appointment.builder()
-    .patientId(patientId)
-    .doctorId(doctorId)
-    .slotId(slotId)
-    .reason("no reason")
-    .appointmentDateTime(now)
-    .build();
+    appointment = Appointment.builder()
+        .patientId(patientId)
+        .doctorId(doctorId)
+        .slotId(slotId)
+        .reason("no reason")
+        .appointmentDateTime(now)
+        .build();
 
-  request = new CreateAppointmentRequest(patientId, doctorId, slotId, "no reason", now);
- }
+    request = new CreateAppointmentRequest(patientId, doctorId, slotId, "no reason", now);
+  }
 
- @Test
- void createAppointment() {
-  when(patientStub.checkPatientExists(any())).thenReturn(
+  @Test
+  void createAppointment() {
+    when(patientStub.checkPatientExists(any())).thenReturn(
         PatientResponse.newBuilder().setExists(true).build());
-  when(doctorStub.checkDoctorExists(any())).thenReturn(DoctorResponse.newBuilder().setExists(true).build());
-  when(repository.save(any(Appointment.class))).thenReturn(appointment);
+    when(doctorStub.checkDoctorExists(any())).thenReturn(DoctorResponse.newBuilder().setExists(true).build());
+    when(repository.save(any(Appointment.class))).thenReturn(appointment);
 
-  AppointmentResponse createdAppointment = service.createAppointment(request);
+    AppointmentResponse createdAppointment = service.createAppointment(request);
 
-  Assertions.assertEquals(request.appointmentTime(), createdAppointment.appointmentDateTime());
-  verify(repository).save(any(Appointment.class));
- }
+    Assertions.assertEquals(request.appointmentTime(), createdAppointment.appointmentDateTime());
+    verify(repository).save(any(Appointment.class));
+  }
 
+  @Test
+  void getAppointmentByIdTest() {
+    UUID id = UUID.randomUUID();
+    when(repository.findById(id)).thenReturn(Optional.of(appointment));
 
- @Test
- void getAppointmentByIdTest()
- {
-  UUID id = UUID.randomUUID();
-  when(repository.findById(id)).thenReturn(Optional.of(appointment));
+    AppointmentResponse response = service.getAppointmentById(id);
 
-  AppointmentResponse response = service.getAppointmentById(id);
+    Assertions.assertEquals(appointment.getAppointmentDateTime(), response.appointmentDateTime());
 
-  Assertions.assertEquals(appointment.getAppointmentDateTime(), response.appointmentDateTime());
+    verify(repository).findById(id);
+  }
 
-  verify(repository).findById(id);
- }
+  @Test
+  void getDoctorById() {
+    UUID doctorId = UUID.randomUUID();
+    List<Appointment> appointments = List.of(appointment);
+    Page<Appointment> appList = new PageImpl<>(appointments);
+    when(repository.findByDoctorId(any(UUID.class), any(Pageable.class))).thenReturn(appList);
 
- @Test
- void getDoctorById()
- {
-  UUID doctorId = UUID.randomUUID();
-  List<Appointment> appointments = List.of(appointment);
-  Page<Appointment> appList = new PageImpl<>(appointments);
-  when(repository.findByDoctorId(any(UUID.class), any(Pageable.class))).thenReturn(appList);
-  
-  Page<AppointmentResponse> saveAppoint = service.getAppointmentsByDoctor(doctorId, 0, 10);
-  
-  Assertions.assertEquals(appointments.get(0).getAppointmentDateTime(), saveAppoint.getContent().get(0).appointmentDateTime());
+    Page<AppointmentResponse> saveAppoint = service.getAppointmentsByDoctor(doctorId, 0, 10);
 
-  verify(repository).findByDoctorId(any(UUID.class), any(Pageable.class));
- }
+    Assertions.assertEquals(appointments.get(0).getAppointmentDateTime(),
+        saveAppoint.getContent().get(0).appointmentDateTime());
+
+    verify(repository).findByDoctorId(any(UUID.class), any(Pageable.class));
+  }
+
+  @Test
+  void testAppointmentResponse()
+      throws NoSuchMethodException, SecurityException, IllegalAccessException, InvocationTargetException {
+    Method toResponse = AppointmentService.class.getDeclaredMethod("toResponse", Appointment.class);
+
+    toResponse.setAccessible(true);
+
+    AppointmentResponse appointmentResponse = (AppointmentResponse) toResponse.invoke(service, appointment);
+
+    Assertions.assertEquals(appointmentResponse.doctorId(), appointment.getDoctorId());
+  }
 
 }
